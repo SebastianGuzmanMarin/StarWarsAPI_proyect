22cf61c64d005139eed7bc4d1a1a3f893830f56d
Sebastian Guzman <sebas_guzman01@hotmail.com>
2025-05-20 10:22:56 -0500

    first commite

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        ConsultaAPIPelicula consulta = new ConsultaAPIPelicula();
+        System.out.println("escribe el numero de la pelicula que quieres consultar");
+        try{
+            var numeroDePelicula = Integer.valueOf(lectura.nextLine());
+            Pelicula pelicula = consulta.buscaPelicula(numeroDePelicula);
+            System.out.println(pelicula);
+            GeneradorDeArchivo generador = new GeneradorDeArchivo();
+            generador.guardarJson(pelicula);
+        }catch (NumberFormatException e){
+            System.out.println("numero no encontrado "+ e.getMessage());
+        }
+        catch (RuntimeException | IOException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando la aplicacion.");
+        }
+
+    }
+}
